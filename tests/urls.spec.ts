bf5ff01f6e53f2942dc183044ff10269b3e3b660
Thor Brink <thor.brink@helsingborg.se>
2024-11-26 14:12:51 +0100

    feat: make workflow reusable

--- tests/urls.spec.ts
+++ tests/urls.spec.ts
@@ -5,7 +5,7 @@ import path from 'path';
 const urls: string[] = fs.readFileSync(path.resolve(__dirname, '../fixtures/sitemap-urls.txt'), 'utf-8').split('\n');
 
 urls.forEach((url: string) => {
-  test(`Response code was 200: ${url}`, async ({ page }) => {
+  test(`Page is OK: ${url}`, async ({ page }) => {
     const response = await page.goto(`${url}`, { waitUntil: 'domcontentloaded' });
     expect(response?.status()).toBe(200);
     const renderErrorCount = await page.getByText('A view rendering issue has occurred').count();
