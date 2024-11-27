1af68905a3e9ef478b8764f7e69107a13d0b1f73
Thor Brink <thor.brink@helsingborg.se>
2024-11-27 07:02:26 +0000

    wip

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ const config: PlaywrightTestConfig = {
   workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: process.env.CI ? 'html' : 'line',
-  reporter: process.env.CI ? 'blob' : 'list',
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
