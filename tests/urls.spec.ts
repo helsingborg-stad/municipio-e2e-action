6be54bab2b57873deb60895f772e66e33ad5bd8d
Thor Brink <thor.brink@helsingborg.se>
2024-11-27 07:06:22 +0000

    wip

--- tests/urls.spec.ts
+++ tests/urls.spec.ts
@@ -4,7 +4,9 @@ import path from 'path';
 
 const urls: string[] = fs.readFileSync(path.resolve(__dirname, '../fixtures/sitemap-urls.txt'), 'utf-8').split('\n');
 
-urls.forEach((url: string) => {
+const uniqueUrls = Array.from(new Set(urls.filter(url => url.trim() !== '')));
+
+uniqueUrls.forEach((url: string) => {
   test(`Page is OK: ${url}`, async ({ page }) => {
     const response = await page.goto(`${url}`, { waitUntil: 'domcontentloaded' });
     expect(response?.status()).toBe(200);
